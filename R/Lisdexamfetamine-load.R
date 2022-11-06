ab35f3b6be8ef24892762e6d83b9aac6c9841d6f
ClaudiaBruno <claudiamarybruno@gmail.com>
2022-11-07 09:53:16 +1100

    Adding R code back to Git?

---
+++ R/Lisdexamfetamine-load.R
@@ -0,0 +1,103 @@
+####
+# Load PBS ADHD data into RDS
+
+#PBSDIR="/mnt/pearson/PBS 10% Sample/Source Data/"
+
+#comment out Malcolm directory - unsure how to make this part more reproducible
+PBSDIR="Y:/PBS 10% Sample/Source Data/"
+
+########################################
+#load libraries
+library(haven)
+library(dplyr)
+library(lubridate)
+library(sessioninfo)
+library(data.table)
+
+session_info()
+time_start<-Sys.time()
+
+# log current git commit
+system('git --no-pager log -1 --date=short --no-decorate --pretty=format:"%d @ %h %cd" HEAD')
+message()
+
+pt <- read_sas(paste0(PBSDIR,"patient_ids.sas7bdat"))
+setDT(pt)
+setkey(pt, PAT_ID)
+
+pt[, dob :=  as.Date(paste0(YEAR_BIRTH, '-07-01'))]
+#pt[, c("YEAR_BIRTH", "YEAR_DEATH", "flag_100", "date_100") := NULL]
+
+agec <- function(a, b) {
+  age<-floor(time_length(interval(as.Date(a), as.Date(b)), "years"))
+  ifelse(age<0,0,ifelse(age>100,100,age))
+}
+
+ADHD <- NULL
+
+for(yr in c("2015", "2016", "2017", "2018", "2019", "2020", "2021", "2022")) {
+  message(paste0("Reading PBS data: ", yr))
+  pbs <- read_sas(paste0(PBSDIR, "y", yr, "supply.sas7bdat"),
+                  paste0(PBSDIR, "formats.sas7bcat"),
+                  col_select=c("SUPP_DATE", "SCRIPTS", "ATC_CODE",
+                               "FORM_CAT", "PAT_ID", "PHARMACY_STATE",
+                               "CONCESSIONAL_STATUS", "ITEM_CODE", "QTY",
+                               "PRESC_ID", "AUTH_FORM_ID", "STREAM_AUTH_CODE", "PRESC_DATE"
+                                 ))
+  message(paste0("    Modified: ",
+                 file.info(paste0(PBSDIR, "y", yr, "supply.sas7bdat"))$mtime))
+  message(paste0("    Rows: ", nrow(pbs)))
+  
+  setDT(pbs)
+  
+  # XXXX can we optimise the subset part?
+  ##I have been using bread() R package to subset on ATC_code and select columns, but not sure if it works on SAS files - not necessarily quicker but I cant load such large datasets on my 16G RAM computer
+  
+  ##not sure if grep or grepl is any quicker -- I thought Bread uses grepl
+  
+  ##I have had some issues with bread if data is messy (i.e, if an ATC code was recorded as C02,AC02 it would move the second half to the next column); 
+  
+  ADHD_tmp <- pbs[substr(ATC_CODE,1,5)=='N06BA'|substr(ATC_CODE,1,7)=='C02AC02']
+ 
+   rm(pbs)
+ 
+   ADHD_tmp[pt, c("AGE", "PATIENT_SEX") := .(agec(dob, SUPP_DATE),
+                                        PATIENT_SEX), on="PAT_ID"]
+  
+   ADHD <- rbind(ADHD, ADHD_tmp)
+   rm(ADHD_tmp)
+}
+
+auth <- read_sas(paste0(PBSDIR, "authority.sas7bdat"))
+setDT(auth)
+setkey(auth, PAT_ID)
+
+ADHD_auth<- merge(x=ADHD, y=auth,  by.x = c('PAT_ID', 'PRESC_DATE', 'ITEM_CODE'),
+                                      by.y = c('PAT_ID', 'AUTH_APPR_DTE','AUTH_ITEM_CODE'), 
+                  all.x = TRUE)
+
+
+#dpotential ata checks - 
+#check duplicate records?
+#check indication and medicine match - need historical restriction numbers:
+#Noting current Authority codes for (can extract and compare with PBS RestrictionExtract text tables from PBS):
+#narcolepsy 1236, 6227, 5983,6017, 6250, 6547, 8694, 10935, 10967, 10968, 10970)
+#ADHD = 6226, 6249, 6279, 6298, 4578, 5252, 7876, 7890, 8544, 8585, 9031, 9034, 10717, 10719, 10792 -> age criteria)
+#number of missing rstr number
+
+#not working as expected
+check <- cbind(ADHD_auth, duplicated(ADHD_auth, by = c("PAT_ID",  "ITEM_CODE", "SUPP_DATE"))) %>% 
+  filter(V2==TRUE) %>% arrange(PAT_ID, SUPP_DATE, ITEM_CODE)
+
+#create new rstr_num : some streamline auth codes where restr_num missing
+#some cases where rstr_num not equal to stream_code
+ check2 <- ADHD_auth[, REST_AUTH_NUM:= fifelse(is.na(RSTR_NUM), STREAM_AUTH_CODE, RSTR_NUM)] %>% 
+.[, .N, by = .(ATC_CODE, REST_AUTH_NUM)] %>% arrange(ATC_CODE, REST_AUTH_NUM) 
+
+
+ 
+saveRDS(ADHD_auth, file = "data/ADHD.Rds")
+
+time_end<- Sys.time()
+
+time_end - time_start
---
+++ R/Lisdexamfetamine-trends.R
@@ -0,0 +1,261 @@
+#################################################################################
+# Project: 	Code for "Changes in lisdexamfetamine after PBS subsidy restriction 
+#  change: Research Letter"				#
+# Author:		Claudia Bruno (Code review by Malcolm Gillies)		            			#
+#           School of Population Health                                      #
+#           UNSW Sydney							                                    	#
+print(paste0("Last updated ", Sys.Date))			                                #
+#################################################################################
+#add libraries based on ARIMA code given in 
+#"Interrupted time series analysis using Autoregressive #
+#           Integrated Moving Average (ARIMA) models: A guide for evaluating 	#
+#	          large-scale health interventions"
+#required libraries for Andrea code
+library(astsa)
+library(forecast)
+library(dplyr)
+library(zoo)
+#ARIMA libraries tidyvert
+library(fable)
+library(tsibble)
+
+#load libraries
+library(magrittr)
+library(ggplot2)
+library(haven)
+library(dplyr)
+library(lubridate)
+library(sessioninfo)
+library(DBI)
+library(data.table)
+
+session_info()
+
+# Create output
+
+
+# log current git commit
+system('git --no-pager log -1 --date=short --no-decorate --pretty=format:"%d @ %h %cd" HEAD')
+message()
+
+ADHD <- readRDS(file = "data/ADHD.rds")
+
+setnames(ADHD, names(ADHD), tolower(names(ADHD)))
+
+# suppress unreliable recent PBS data
+ADHD %<>% filter(supp_date < as.Date("2022-07-01"))
+
+
+##functions and themes to be used
+#themes for graphs
+theme_Publication <- function(base_size=14, base_family="sans") {
+  library(grid)
+  library(ggthemes)
+  (theme_foundation(base_size=base_size, base_family=base_family)
+    + theme(plot.title = element_text(face = "bold",
+                                      size = rel(1.25), hjust = 0.5),
+            text = element_text(),
+            panel.background = element_rect(colour = NA),
+            plot.background = element_rect(colour = NA),
+            panel.border = element_rect(colour = NA),
+            axis.title = element_text(face = "bold",size = rel(1)),
+            axis.title.y = element_text(angle=90,vjust =2),
+            axis.title.x = element_text(vjust = -0.2),
+            axis.text = element_text(), 
+            axis.line.x = element_line(colour="black"),
+            axis.line.y = element_line(colour="black"),
+            axis.ticks = element_line(),
+            panel.grid.major = element_line(colour="#f0f0f0"),
+            panel.grid.minor = element_blank(),
+            legend.key = element_rect(colour = NA),
+            legend.position = "bottom",
+            legend.direction = "horizontal",
+            legend.key.size= unit(0.6, "cm"),
+            #legend.margin = unit(0.2, "cm"),
+            legend.title = element_text(),
+            plot.margin=unit(c(10,5,5,5),"mm"),
+            strip.background=element_rect(colour="#f0f0f0",fill="#f0f0f0"),
+            strip.text = element_text(face="bold")
+    ))
+  
+}
+
+
+#Prepare data for following analysis
+#create new age group function for children and adults
+agegrp<- function(age){
+  case_when(age  %between%  c(-130,17) ~ "Children (0-17 years)",
+            age >=18 ~ "Adults (\u2265 18 years)")
+}
+##################################
+
+#import PBS data
+
+
+#create age_group, year, sex, and month of supply date - group total by monthly
+#add class and medicine variable
+
+
+ADHD[, ':=' (
+  age_group = agegrp(age), 
+  year = lubridate::year(supp_date),
+  sex = if_else(patient_sex == "M", "Male", "Female"), 
+  mos = lubridate::ymd(paste(year(supp_date), month(supp_date), "01")), 
+  
+  medicine=fcase(
+    atc_code == 'C02AC02', 'Nonstimulant',
+    atc_code == 'N06BA02', 'Stimulant',
+    atc_code == 'N06BA04', 'Stimulant',
+    atc_code == 'N06BA09', 'Nonstimulant',
+    atc_code == 'N06BA12', 'Lisdexamfetamine',
+    default = "Other"),  
+  
+  medicine = fcase(
+    atc_code == 'C02AC02', 'Guanfacine',
+    atc_code == 'N06BA02', 'Dexamfetamine',
+    atc_code == 'N06BA04', 'Methylphenidate',
+    atc_code == 'N06BA09', 'Atomoxetine',
+    atc_code == 'N06BA12', 'Lisdexamfetamine',
+    default = "Other"))] %>%  #other includes modafinil and armodafinil 
+  .[, .(pat_id, supp_date, mos, year,
+        item_code, 
+        atc_code, medicine, medicine, age_group, sex)]
+
+
+# Convert data to time series object - dispensings
+disp_medicine<- 
+  ADHD[age>17, .N, by = .(medicine, mos)] %>% 
+  arrange( medicine, mos) 
+
+#for now i will just convert and run analysis on lisdexamfetamine only
+
+disp_ts <- disp_medicine %>% 
+  filter(medicine == "Lisdexamfetamine") %>% 
+  select(N) %>% 
+  ts(frequency=12, start=c(2016,1))
+
+# Step 1: Plot data to visualise time series
+options(scipen=5)
+
+maxy <- max(disp_medicine$N[disp_medicine$medicine == "Lisdexamfetamine"])
+plot(disp_ts, ylim=c(0,maxy+1000), type='l', col="blue", xlab="Month", ylab="Dispensings")
+# Add vertical line indicating date of intervention (Feb 1, 2021)
+abline(v=2021.083, col="gray", lty="dashed", lwd=2)
+
+# Step 2. Transform data to stabilise variance (if necessary). If the variance is changing over time, a log-transformation should be applied.
+
+
+# Step 3. Model selection: While automated algorithms in several statistical packages can identify candidate p and q parameters, they can sometimes be estimated based on the ACF/PACF plots.
+#a. Determine differencing order to induce stationary: If there is seasonality, a seasonal difference is required and D =1.
+#b. Plot the ACF/PACF of stationarity data to determine potential AR/MA orders
+#c. Estimate model and use information criteria to find the best model:
+
+# Step 3a. View ACF/PACF plots of undifferenced data
+acf2(disp_ts, max.lag=24)
+
+# Step 3a. View ACF/PACF plots of differenced/seasonally differenced data
+acf2(diff(diff(disp_ts,12)), max.lag=24)
+
+intervention_date = 'Feb 2021'
+# Create variable representing step change and view
+step <- as.numeric(as.yearmon(time(disp_ts))>=intervention_date)
+step
+
+# Create variable representing ramp (change in slope) and view
+#edited from rep(0, 36), seq(1, 12, 1) <- assume this ramp begins at intervention
+ramp <- append(rep(0,length(step[step < 1])), seq(1,length(step[step == 1]),1))
+ramp  
+
+#c. Estimate model and use information criteria to find the best model:
+
+# Use automated algorithm to identify p/q parameters
+# Specify first difference = 1 and seasonal difference = 1
+model1 <- auto.arima(disp_ts, seasonal=TRUE, xreg=cbind(step,ramp), max.d=1, max.D=1, stepwise=FALSE, trace=TRUE)
+
+#Automated best model: Regression with ARIMA(0,1,4)(0,0,1)[12] errors
+#results seemed not to include any models with seasonal difference =1
+
+#Autocorrelation order of the model (P) is 0, the moving average order of the model q = 4;
+#Autocorrelation order of the seasonal model (P) is 0, the moving average order of the seasonal model Q = 1;
+
+
+#creating a model 2 - which adds Seasonal difference D = 1
+model2 <- Arima(disp_ts, xreg=cbind(step,ramp),order = c(0, 1, 4),
+                seasonal = list(order = c(0, 1, 1) ))
+
+#Step 4. Check if residuals of chosen model are white noise.
+
+# Check residuals
+checkresiduals(model1)
+checkresiduals(model2)
+#AFC plot model 1 lag crosses - 0.2; There is no obvious pattern or significant autocorrelation in the residuals, and they are normally distributed. resdiuals look normally distributed
+#Model 2 There is no obvious pattern or significant autocorrelation in the residuals, and they are normally distributed. resdiuals look normally distributed
+
+Box.test(model1$residuals, lag = 24, type = "Ljung-Box")
+Box.test(model2$residuals, lag = 24, type = "Ljung-Box")
+
+#The p-value for the Ljung-Box test for white noise is 0.17 (model 1) and 0.69 (model2) at 24 lags. 
+#As the null hypothesis for the Ljung-Box test is that there is no significant autocorrelation, we do not reject the null and our chosen model has a good fit.The p-value for the Ljung-Box test for white noise is 0.50 at 24 lags. As the null hypothesis for the Ljung-Box test is that there is no significant autocorrelation, we do not reject the null and our chosen model has a good fit. 
+
+# Estimate parameters and confidence intervals
+summary(model1)
+confint(model1)
+#AIC=971.29   AICc=973.27   BIC=990.55
+summary(model2)
+confint(model2)
+
+#model2 has lowest information criterion so will choose that model
+#AIC=818.48   AICc=820.85   BIC=836.47
+
+
+# To forecast the counterfactual, model data excluding post-intervention time period
+model3 <- Arima(window(disp_ts, end=c(2021,1)), order=c(0,1,4), seasonal=list(order=c(0,1,1), period=18))
+
+# Forecast 12 months post-intervention and convert to time series object
+fc <- forecast(model3, h=18)%>%
+  autoplot()
+fc.ts <- ts(as.numeric(fc$mean), start=c(2021,2), frequency=18)
+
+# Combine with observed data
+#Bind time series which have a common frequency. ts.union pads with NAs to the total time coverage, ts.intersect restricts to the time covered by all the series.
+disp_ts.2 <- ts.union(disp_ts, fc.ts)
+disp_ts.2
+
+# Plot 
+plot(disp_ts.2, type="l", plot.type="s", col=c('blue','red'), xlab="Month", ylab="Dispensings", 
+     #linetype=c("solid","dashed"), #getting error here
+     
+     ylim=c(0,maxy+50), 
+     lwd=2)
+abline(v=2021.083, col="gray", lty="dashed", lwd=2)
+##end of Andrea's papers R code
+
+##Forecast uses autoplot which uses ggplot - needs updating - ideally also plot 
+
+disp_lis <- disp_medicine %>% filter(medicine == "Lisdexamfetamine") %>% arrange(mos) %>% 
+  mutate(time =time(disp_ts), series= "disp_ts")
+
+autoplot(disp_ts.2)+
+  geom_point(data = disp_lis, aes(x=time, y=N))+
+  labs(x="Date (Month Year)", y= "Number of dispensings", title= "Lisdexamfetamine dispensings with ARIMA forcast among adults") +
+  scale_y_continuous(labels = scales::comma)+
+  #facet_grid(. ~ age_group, margins=FALSE, scales = "free_y") +
+  theme_Publication() +
+  #scale_x_date(expand = c(0.04,0.04), date_minor_breaks = "3 month", date_labels = "%b %y", date_breaks = "6 month",) +
+  geom_vline(xintercept = 2021.083, linetype=4, size=1.1)+ theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
+
+
+
+#########################################
+#Number of new users by month and age group - 2016 to 2022
+#prepare new users dataset
+
+ADHD_new <- ADHD[order(pat_id, supp_date)] %>% .[rowid(pat_id) == 1,] %>% 
+  .[ year >= 2016,] %>% 
+  .[, .(pat_id,  sex, mos, medicine, class, age_group)]
+
+
+
+
+##should we include - new use by medicine: then compare adults who initiated treatment before the change then intiated another medicine following subsidy?
+
